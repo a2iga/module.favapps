1160bec6b958d21d7e45b9e6edc8e9ad5adc43ee
rx1310 <rx1310@inbox.ru>
2021-04-25 18:01:05 +0300

    Небольшие правки

--- app/src/main/java/ru/rx1310/a2iga/module/favapps/LaunchModule.java
+++ app/src/main/java/ru/rx1310/a2iga/module/favapps/LaunchModule.java
@@ -9,6 +9,7 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.graphics.drawable.BitmapDrawable;
+import android.content.pm.ApplicationInfo;
 
 public class LaunchModule extends PreferenceActivity {
 
@@ -17,27 +18,36 @@ public class LaunchModule extends PreferenceActivity {
                appItem3, 
                appItem4;
                
+    String pkgNameApp1,
+           pkgNameApp2,
+           pkgNameApp3,
+           pkgNameApp4;
+               
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         PreferenceScreen mPreferenceScreen = getPreferenceManager().createPreferenceScreen(this);
         setPreferenceScreen(mPreferenceScreen);
-
+        //getActionBar().hide();
+        
         // Preference item for App1
         appItem1 = new Preference(this);
         appItem1.setTitle("App 1");
-        appItem1.setSummary("Summary for App 1");
+        appItem1.setSummary(getAppName(this, "ru.rx1310.app.a2iga"));
+        appItem1.setIcon(getAppIcon(this, "ru.rx1310.app.a2iga"));
         
         // Preference item for App2
         appItem2 = new Preference(this);
         appItem2.setTitle("App 2");
-        appItem2.setSummary("Summary for App 2");
+        appItem2.setSummary(getAppName(this, "com.android.settings"));
+        appItem2.setIcon(getAppIcon(this, "com.android.settings"));
         
         // Preference item for App3
         appItem3 = new Preference(this);
         appItem3.setTitle("App 3");
-        appItem3.setSummary("Summary for App 3");
+        appItem3.setSummary(getAppName(this, "com.niksoftware.snapseed"));
+        appItem3.setIcon(getAppIcon(this, "com.niksoftware.snapseed"));
         
         // Preference item for App4
         appItem4 = new Preference(this);
@@ -50,6 +60,35 @@ public class LaunchModule extends PreferenceActivity {
         mPreferenceScreen.addPreference(appItem3);
         mPreferenceScreen.addPreference(appItem4);
 
+    }
+    
+    // ? Получение имени приложения
+    public static String getAppName(Context context, String packageName) {
+
+        try {
+
+            PackageManager pm = context.getPackageManager();
+            ApplicationInfo ai = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+
+            String appName = (String) pm.getApplicationLabel(ai);
+
+            return appName;
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            //Log(context, "e", "getAppName: " + e);
+            return "e: getAppName();";
+        }
+
+	}
+    
+    public static Drawable getAppIcon(Context context, String pkgName) {
+        try {
+            return context.getPackageManager().getApplicationIcon(pkgName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
     
 }
